f89ab4caf3eede9c714dd721546b6341c1e96ceb
phwt <phwt.smwt@gmail.com>
2020-04-17 23:36:46 +0700

    Add 2 spaces between def

--- document/views.py
+++ document/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from document.models import Document, InternalDoc, ExternalDoc
 from django.shortcuts import redirect, render
 
+
 def index(request):
     return render(request, 'index.html')
     # return HttpResponse("work")
@@ -23,14 +24,21 @@ def external_doc(request):
         'type': 'external'
     }
     return render(request, 'listpage.html', context=context)
+
+
 def internal_detail(request, id):
     doc = InternalDoc.objects.get(document_ptr_id=id)
     return render(request, template_name='internal_detail.html', context={
         'doc': doc
     })
 
+
 def external_detail(request, id):
     doc = ExternalDoc.objects.get(document_ptr_id=id)
     return render(request, template_name='external_detail.html', context={
         'doc': doc
     })
+
+
+def external_add(request):
+    return None

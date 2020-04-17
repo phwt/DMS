550b6b3504377305a91510973aacf9f038aaf0e0
phwt <phwt.smwt@gmail.com>
2020-04-17 22:43:16 +0700

    Seperate detail url

--- document/urls.py
+++ document/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index),
     path('internal/', views.internal_doc),
     path('external/', views.external_doc),
-    path('detail/<int:id>/', views.detail),
+    path('internal/detail/<int:id>/', views.internal_detail),
+    path('external/detail/<int:id>/', views.external_detail),
     # path('external/add', views.external_add),
 ]

589805da4cde781b1100f2992c6a8e6ef8ad7a13
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2020-04-17 23:03:02 +0700

    internal/external doc templates & views

--- document/views.py
+++ document/views.py
@@ -1,8 +1,19 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from document.models import Document, InternalDoc, ExternalDoc
+from django.shortcuts import redirect, render
 
-
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
     # return HttpResponse("work")
+
+
+def internal_detail(request, id):
+    doc = InternalDoc.objects.get(pk=id)
+    return render(request, template_name='internal_detail.html', context={
+        'doc': doc
+    })
+
+def external_detail(request, id):
+    doc = ExternalDoc.objects.get(pk=id)
+    return render(request, template_name='external_detail.html', context={
+        'doc': doc
+    })

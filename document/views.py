d5034d2f5a4fabf450c4ec75d93b0051991ccc11
maizerocom <41408745+maizerocom@users.noreply.github.com>
2020-04-17 23:12:54 +0700

    Create ListPage

--- document/views.py
+++ document/views.py
@@ -1,8 +1,38 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from document.models import InternalDoc, ExternalDoc
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
     # return HttpResponse("work")
+
+
+def internal_doc(request):
+    internal = InternalDoc.objects.all()
+    context = {
+        'internal': internal,
+        'type': 'internal'
+    }
+    return render(request, 'listpage.html', context=context)
+
+
+def external_doc(request):
+    external = ExternalDoc.objects.all()
+    context = {
+        'external': external,
+        'type': 'external'
+    }
+    return render(request, 'listpage.html', context=context)
+
+
+# def detail(request):
+#     return None
+
+
+def internal_detail(request):
+    return None
+
+
+def external_detail(request):
+    return None

456c8db1a447c32005ded4ab39ae38c450b2f493
phwt <phwt.smwt@gmail.com>
2020-04-17 23:36:37 +0700

    Add name

--- document/urls.py
+++ document/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index),
     path('internal/', views.internal_doc),
     path('external/', views.external_doc),
-    path('internal/detail/<int:id>/', views.internal_detail),
-    path('external/detail/<int:id>/', views.external_detail),
-    # path('external/add', views.external_add),
+    path('internal/detail/<int:id>/', views.internal_detail, name='internal_detail'),
+    path('external/detail/<int:id>/', views.external_detail, name='external_detail'),
+    path('external/add', views.external_add),
 ]

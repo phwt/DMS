db9aca0136a33eeac454bfdce6e12fbff65fc45e
maizerocom <41408745+maizerocom@users.noreply.github.com>
2020-04-19 02:09:45 +0700

    Create Work List

--- work/views.py
+++ work/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from work.models import Work
+
 
 def work_list(request):
-    return None
+    work = Work.objects.all()
+    context = {
+        'works': work
+    }
+    return render(request, 'work_list.html', context=context)
 
 
 def work_detail(request):

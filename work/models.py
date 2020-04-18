5ee9e2a48fb5bcdf70bb6a0ab660c308eb7b2012
phwt <phwt.smwt@gmail.com>
2020-04-19 02:36:50 +0700

    Work models

--- work/models.py
+++ work/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from document.models import InternalDoc
+from authen.models import Employee
+
+
+class DelegateUser(models.Model):
+    users = models.ManyToManyField(Employee, through='Work')
+    result = models.BooleanField()
+    review_date = models.DateTimeField()
+
 
 class Work(models.Model):
     STATUS_CHOICE = [
@@ -18,16 +26,5 @@ class Work(models.Model):
     complete_date = models.DateTimeField()
     create_date = models.DateTimeField(auto_now_add=True)
     document_id = models.ForeignKey(InternalDoc, on_delete=models.CASCADE)
-
-class Department(models.Model):
-    name = models.CharField(max_length=50, null=False)
-
-class Contributor(models.Model):
-    work_id = models.ManyToManyField(Work, through='Employee')
-    employee_id = models.ManyToManyField(Employee, through='Work')
-
-class Delegate_User(models.Model):
-    work_id = models.ManyToManyField(Work, through='Employee')
-    employee_id = models.ManyToManyField(Employee, through='Work')
-    result = models.BooleanField()
-    review_date = models.DateTimeField()
+    employees = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    delegate_user = models.ForeignKey(DelegateUser, on_delete=models.CASCADE)

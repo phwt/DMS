4eca62e413fd26af01faf492920a2f8c26700074
maizerocom <41408745+maizerocom@users.noreply.github.com>
2020-04-18 18:56:20 +0700

    Create Change Password Feature

--- auth/views.py
+++ auth/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.models import User
 
 # Create your views here.
 def auth_login(request):
@@ -13,8 +14,6 @@ def auth_login(request):
         user = authenticate(request, username=username, password=password)
 
         if user:
-            print('aaaaaaaaaaaaaaaaaaaaa')
-            print(user)
             login(request, user)
             return redirect('index')
         else:
@@ -27,3 +26,29 @@ def auth_login(request):
 def auth_logout(request):
     logout(request)
     return redirect('login')
+
+@login_required
+def auth_changepassword(request):
+    context = {}
+    # u = User.objects.get(username='john')
+    # u.set_password('new password')
+    # u.save()
+    if request.method == 'POST':
+        username = request.user.username
+        password = request.POST.get('password')
+        newpassword = request.POST.get('newpassword')
+        confirm = request.POST.get('confirmnewpassword')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            if newpassword == confirm:
+                u = User.objects.get(username=username)
+                u.set_password(newpassword)
+                u.save()
+                return redirect('login')
+            else:
+                context['password'] = password
+                context['errornew'] = 'Password NotMatch!'
+        else:
+            context['password'] = password
+            context['error'] = 'Wrong password!'
+    return render(request, 'changepassword.html', context=context)

090cfd506d93d02979862b48630aeed78473b92e
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2020-04-19 02:06:06 +0700

    work model

--- work/models.py
+++ work/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from document.models import InternalDoc
 
-# Create your models here.
+class Work(models.Model):
+    STATUS_CHOICE = [
+        ('N', 'New'),
+        ('DCC', 'DCC-review'),
+        ('MR', 'MR-review'),
+        ('VP', 'VP-review'),
+        ('SVP', 'SVP-review'),
+        ('C', 'Completed'),
+    ]
+    status = models.CharField(
+        max_length=3,
+        choices=STATUS_CHOICE,
+        null=False,
+    )
+    complete_date = models.DateTimeField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    document_id = models.ForeignKey(InternalDoc, on_delete=models.CASCADE)
+
+class Department(models.Model):
+    name = models.CharField(max_length=50, null=False)
+
+class Contributor(models.Model):
+    work_id = models.ManyToManyField(Work, through='Employee')
+    employee_id = models.ManyToManyField(Employee, through='Work')
+
+class Delegate_User(models.Model):
+    work_id = models.ManyToManyField(Work, through='Employee')
+    employee_id = models.ManyToManyField(Employee, through='Work')
+    result = models.BooleanField()
+    review_date = models.DateTimeField()

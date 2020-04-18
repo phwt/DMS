79c9fcf50cabe62d3c89b17b526a947a482b89a4
phwt <phwt.smwt@gmail.com>
2020-04-19 01:38:24 +0700

    Rename auth to authen

--- DMS/settings.py
+++ DMS/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'document',
     'work',
-    'auth',
+    'authen',
 ]
 
 MIDDLEWARE = [
--- DMS/urls.py
+++ DMS/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('document/', include('document.urls')),
-    path('auth/', include('auth.urls')),
+    path('auth/', include('authen.urls')),
 ]

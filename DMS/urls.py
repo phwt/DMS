ad51677760cdd52c051f9a3eb75ee6a8dc2539be
phwt <phwt.smwt@gmail.com>
2020-04-19 17:07:38 +0700

    Index page

--- DMS/urls.py
+++ DMS/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from document import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     path('document/', include('document.urls')),
     path('auth/', include('authen.urls')),
     path('work/', include('work.urls')),
--- document/urls.py
+++ document/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('internal/', views.document_list, {'doc_type': 'internal'}, name='internal_doc'),
     path('external/', views.document_list, {'doc_type': 'external'}, name='external_doc'),
     path('detail/<int:id>/', views.document_detail, name='doc_detail'),

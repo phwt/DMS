0b4acf27fd5144d9217cb86be8d09a9db6c2d8c5
Tummasorn Tantiyapinun <42957781+tangtms@users.noreply.github.com>
2020-04-19 21:43:17 +0700

    form css

--- work/forms.py
+++ work/forms.py
@@ -1,5 +1,4 @@
-from django.forms import ModelForm
-
+from django.forms import ModelForm, TextInput, DateTimeField, CharField, NumberInput, Select, Textarea
 from document.models import InternalDoc
 from work.models import Work
 
@@ -8,6 +7,14 @@ class DocumentCreateForm(ModelForm):
     class Meta:
         model = InternalDoc
         exclude = ['file_location', 'state']
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'version': NumberInput(attrs={'class': 'form-control form-control-sm'}),
+            'running_no': NumberInput(attrs={'class': 'form-control form-control-sm'}),
+            'type': Select(attrs={'class': 'form-control form-control-sm'}),
+            'status': Select(attrs={'class': 'form-control form-control-sm'}),
+            'parent_doc': Select(attrs={'class': 'form-control form-control-sm'}),
+        }
 
     def __init__(self, *args, **kwargs):
         super(DocumentCreateForm, self).__init__(*args, **kwargs)
@@ -18,3 +25,7 @@ class DocumentEditCancelForm(ModelForm):
     class Meta:
         model = Work
         fields = ['document', 'detail']
+        widgets = {
+            'document': TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'detail': Textarea(attrs={'class': 'form-control form-control-sm'}),
+        }

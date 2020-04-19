cdde7294850fc6012ca45e330eb08685c8aaf237
phwt <phwt.smwt@gmail.com>
2020-04-19 15:19:34 +0700

    Remove roles from employee

--- authen/models.py
+++ authen/models.py
@@ -8,12 +8,12 @@ class Department(models.Model):
 
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    ROLES = [
-        ('EMP', 'Employee'),
-        ('DCC', 'Document Control Clerk'),
-        ('MR', 'Management Representative'),
-        ('VP', 'Vice President'),
-        ('SVP', 'Senior Vice President'),
-    ]
-    role = models.CharField(max_length=3, choices=ROLES, null=False, default='EMP')
+    # ROLES = [
+    #     ('EMP', 'Employee'),
+    #     ('DCC', 'Document Control Clerk'),
+    #     ('MR', 'Management Representative'),
+    #     ('VP', 'Vice President'),
+    #     ('SVP', 'Senior Vice President'),
+    # ]
+    # role = models.CharField(max_length=3, choices=ROLES, null=False, default='EMP')
     department = models.ForeignKey(to=Department, on_delete=models.CASCADE, null=False)

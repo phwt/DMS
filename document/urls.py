21ba2c5d56ebd62589e9421e1a9818bb7ea912b0
phwt <phwt.smwt@gmail.com>
2020-04-19 17:02:11 +0700

    Edit navbar

--- document/urls.py
+++ document/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('internal/', views.document_list, {'doc_type': 'internal'}, name='internal_doc'),
     path('external/', views.document_list, {'doc_type': 'external'}, name='external_doc'),
     path('detail/<int:id>/', views.document_detail, name='doc_detail'),
-    path('external/add', views.external_add),
+    path('external/add', views.external_add, name='external_add'),
 ]

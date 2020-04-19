2e7f2afaf7284749ad6f092bda694c6289232d5d
maizerocom <41408745+maizerocom@users.noreply.github.com>
2020-04-19 19:16:21 +0700

    Update Work List

--- work/views.py
+++ work/views.py
@@ -5,11 +5,11 @@ from work.models import Work
 
 
 def work_list(request):
-    # work = Work.objects.all()
-    # context = {
-    #     'works': work
-    # }
-    return render(request, 'work_list.html')
+    work = Work.objects.all()
+    context = {
+        'works': work
+    }
+    return render(request, 'work_list.html', context=context)
 
 
 def work_detail(request):

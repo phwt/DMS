b537fda4cecefcf10d66c85133d5081b55295376
phwt <phwt.smwt@gmail.com>
2020-04-19 21:59:43 +0700

    Update models.py

--- authen/models.py
+++ authen/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 class Department(models.Model):
     name = models.CharField(max_length=50, null=False)
 
+    def __str__(self):
+        return self.name
 
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -17,3 +19,6 @@ class Employee(models.Model):
     # ]
     # role = models.CharField(max_length=3, choices=ROLES, null=False, default='EMP')
     department = models.ForeignKey(to=Department, on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'

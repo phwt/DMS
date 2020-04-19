ced059b21f7ec5768907fb91c6a4fcf92d242195
phwt <phwt.smwt@gmail.com>
2020-04-19 21:03:30 +0700

    Serve media files

--- DMS/urls.py
+++ DMS/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from document import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
@@ -24,3 +27,6 @@ urlpatterns = [
     path('auth/', include('authen.urls')),
     path('work/', include('work.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

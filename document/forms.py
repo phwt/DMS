373b21bfcb818a9c6fd068e57cbdfe5558cb4a0b
phwt <phwt.smwt@gmail.com>
2020-04-22 20:12:09 +0700

    Chane InternalDocFilterForm into normal form

--- document/forms.py
+++ document/forms.py
@@ -1,17 +1,17 @@
 from datetime import datetime
 
-from django.forms import ModelForm, TextInput, DateTimeField, CharField, NumberInput, Select, Textarea, FileInput
+from django import forms
 
 from DMS.utils import all_field_required_false, apply_class_to_fields
 from document.models import ExternalDoc, InternalDoc
 
 
-class ExternalDocForm(ModelForm):
+class ExternalDocForm(forms.ModelForm):
     class Meta:
         model = ExternalDoc
         fields = ['name', 'source', 'detail', 'file_location']
         widgets = {
-            'source': TextInput()
+            'source': forms.TextInput()
         }
 
     def __init__(self, *args, **kwargs):
@@ -19,13 +19,13 @@ class ExternalDocForm(ModelForm):
         apply_class_to_fields(self.fields, 'form-control form-control-sm')
 
 
-class ExternalDocFilterForm(ModelForm):
+class ExternalDocFilterForm(forms.ModelForm):
     class Meta:
         model = ExternalDoc
         fields = ['name', 'source', 'detail']
         widgets = {
-            'source': TextInput(),
-            'detail': TextInput(),
+            'source': forms.TextInput(),
+            'detail': forms.TextInput(),
         }
 
     def __init__(self, *args, **kwargs):
@@ -34,24 +34,46 @@ class ExternalDocFilterForm(ModelForm):
         apply_class_to_fields(self.fields, 'form-control form-control-sm')
 
 
-class InternalDocFilterForm(ModelForm):
-    # created_start = DateTimeField()
-    # created_end = DateTimeField()
-    released_start = DateTimeField(
-        initial='1970-01-01T00:00',
-        input_formats=('%Y-%m-%dT%H:%M',)
-    )
-    released_end = DateTimeField(
-        initial=datetime.now().strftime('%Y-%m-%dT%H:%M'),
-        input_formats=('%Y-%m-%dT%H:%M',)
-    )
-    # parent_doc_name = CharField()
+# class InternalDocFilterForm(forms.ModelForm):
+#     # created_start = DateTimeField()
+#     # created_end = DateTimeField()
+#     released_start = forms.DateTimeField(
+#         initial='1970-01-01T00:00',
+#         input_formats=('%Y-%m-%dT%H:%M',)
+#     )
+#     released_end = forms.DateTimeField(
+#         initial=datetime.now().strftime('%Y-%m-%dT%H:%M'),
+#         input_formats=('%Y-%m-%dT%H:%M',)
+#     )
+#     # parent_doc_name = CharField()
+#
+#     class Meta:
+#         model = InternalDoc
+#         exclude = ['file_location', 'release_date', 'create_date']
+#
+#     def __init__(self, *args, **kwargs):
+#         super(InternalDocFilterForm, self).__init__(*args, **kwargs)
+#         all_field_required_false(self.fields)
+#         apply_class_to_fields(self.fields, 'form-control form-control-sm')
 
-    class Meta:
-        model = InternalDoc
-        exclude = ['file_location', 'release_date', 'create_date']
+
+class InternalDocFilterForm(forms.Form):
+    name = forms.CharField()
+    version = forms.IntegerField()
+    running_no = forms.IntegerField()
+    type = forms.ChoiceField(choices=[('', '----')]+InternalDoc.TYPES)
+    state = forms.ChoiceField(choices=[('', '----')]+InternalDoc.STATES)
+    parent_doc = forms.ModelChoiceField(queryset=InternalDoc.objects.all())
 
     def __init__(self, *args, **kwargs):
         super(InternalDocFilterForm, self).__init__(*args, **kwargs)
         all_field_required_false(self.fields)
-        apply_class_to_fields(self.fields, 'form-control form-control-sm')
+        apply_class_to_fields(self.fields, 'form-control form-control-sm', exclude=['parent_doc'])
+
+    select_option = {
+        'class': 'form-control form-control-sm selectpicker',
+        'data-live-search': 'true',
+        'data-style': 'form-control border'
+    }
+
+    parent_doc.widget.attrs.update(select_option)

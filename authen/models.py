9d06235e64c39c2de42b9af13b1b2702388b636a
phwt <phwt.smwt@gmail.com>
2020-04-24 15:16:56 +0700

    Add abbreviation

--- authen/models.py
+++ authen/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Department(models.Model):
     name = models.CharField(max_length=50, null=False)
+    abbreviation = models.CharField(max_length=2, null=False)
 
     def __str__(self):
         return self.name
@@ -11,14 +12,6 @@ class Department(models.Model):
 
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # ROLES = [
-    #     ('EMP', 'Employee'),
-    #     ('DCC', 'Document Control Clerk'),
-    #     ('MR', 'Management Representative'),
-    #     ('VP', 'Vice President'),
-    #     ('SVP', 'Senior Vice President'),
-    # ]
-    # role = models.CharField(max_length=3, choices=ROLES, null=False, default='EMP')
     department = models.ForeignKey(to=Department, on_delete=models.CASCADE, null=False)
 
     def __str__(self):

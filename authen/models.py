08ff9630dcc2206d5c07b0addfc4da978a26eae1
phwt <phwt.smwt@gmail.com>
2020-04-25 15:08:49 +0700

    Add indexes

--- authen/models.py
+++ authen/models.py
@@ -3,16 +3,16 @@ from django.contrib.auth.models import User
 
 
 class Department(models.Model):
-    name = models.CharField(max_length=50, null=False)
-    abbreviation = models.CharField(max_length=2, null=False)
+    name = models.CharField(max_length=50, null=False, db_index=True)
+    abbreviation = models.CharField(max_length=2, null=False, db_index=True)
 
     def __str__(self):
         return self.name
 
 
 class Employee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    department = models.ForeignKey(to=Department, on_delete=models.CASCADE, null=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, db_index=True)
+    department = models.ForeignKey(to=Department, on_delete=models.CASCADE, null=False, db_index=True)
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'

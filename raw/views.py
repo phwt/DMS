119e1156a5e96c19b9164a9a7af051b0b228903b
phwt <phwt.smwt@gmail.com>
2020-04-29 22:09:09 +0700

    Fix regex

--- raw/views.py
+++ raw/views.py
@@ -1,35 +1,36 @@
 from django.contrib.auth.decorators import login_required
 from django.db import connection
 from django.shortcuts import render
+import re
 
 
 @login_required(login_url='login')
 def index(request):
     query = request.POST.get('query', '')
     if request.method == 'POST':
-        if 'CREATE' in query.upper():
+        if bool(re.search(r'\bCREATE\b', query, flags=re.IGNORECASE)):
             return render(request, 'raw.html', {'error': 'CREATE', 'query': query})
-        if 'ALTER' in query.upper():
+        if bool(re.search(r'\bALTER\b', query, flags=re.IGNORECASE)):
             return render(request, 'raw.html', {'error': 'ALTER', 'query': query})
-        if 'DROP' in query.upper():
+        if bool(re.search(r'\bDROP\b', query, flags=re.IGNORECASE)):
             return render(request, 'raw.html', {'error': 'DROP', 'query': query})
-        if 'TRUNCATE' in query.upper():
+        if bool(re.search(r'\bTRUNCATE\b', query, flags=re.IGNORECASE)):
             return render(request, 'raw.html', {'error': 'TRUNCATE', 'query': query})
-        if 'COMMENT' in query.upper():
+        if bool(re.search(r'\bCOMMENT\b', query, flags=re.IGNORECASE)):
             return render(request, 'raw.html', {'error': 'COMMENT', 'query': query})
-        if 'RENAME' in query.upper():
+        if bool(re.search(r'\bRENAME\b', query, flags=re.IGNORECASE)):
             return render(request, 'raw.html', {'error': 'RENAME', 'query': query})
-        if 'INSERT' in query.upper():
+        if bool(re.search(r'\bINSERT\b', query, flags=re.IGNORECASE)):
             return render(request, 'raw.html', {'error': 'INSERT', 'query': query})
-        if 'UPDATE' in query.upper():
+        if bool(re.search(r'\bUPDATE\b', query, flags=re.IGNORECASE)):
             return render(request, 'raw.html', {'error': 'UPDATE', 'query': query})
-        if 'DELETE' in query.upper():
+        if bool(re.search(r'\bDELETE\b', query, flags=re.IGNORECASE)):
             return render(request, 'raw.html', {'error': 'DELETE', 'query': query})
-        if 'MERGE' in query.upper():
+        if bool(re.search(r'\bMERGE\b', query, flags=re.IGNORECASE)):
             return render(request, 'raw.html', {'error': 'MERGE', 'query': query})
-        if 'CALL' in query.upper():
+        if bool(re.search(r'\bCALL\b', query, flags=re.IGNORECASE)):
             return render(request, 'raw.html', {'error': 'CALL', 'query': query})
-        if 'INFORMATION_SCHEMA' in query.upper():
+        if bool(re.search(r'\bINFORMATION_SCHEMA\b', query, flags=re.IGNORECASE)):
             return render(request, 'raw.html', {'error': 'INFORMATION_SCHEMA', 'query': query})
 
         with connection.cursor() as cursor:
